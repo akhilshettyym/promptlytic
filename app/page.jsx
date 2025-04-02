9f814dedf30cc5acd46c1b271c2f1112a26db8b7
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-04-02 21:43:06 +0530

    Basic file structure and components

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,19 @@
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <section className="w-full flex-center felx-col">
+      <h1 className="head_text text-center">
+        Discover and share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center"> AI-Powered Prompts</span>
+        </h1>
+        <p className="desc text-center">
+          Promptlytic is an open-source AI prompting tool for modern world to discover, create and share creative prompts.
+          Explore a vast library of AI-generated prompts, curated by our community of creators and enthusiasts.
+        </p>
+
+         {/* Feed */}
+
+    </section>
   )
 }
 
