35cf105b0e38087f5e562b8e3e39922eecb4a526
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-04-03 19:23:24 +0530

    Api setup

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,6 +11,7 @@ const RootLayout = ({ children }) => {
     return (
         <html lang='en'>
             <body>
+            <Provider>
                 <div className='main'>
                     <div className='gradient' />
                 </div>
@@ -19,7 +20,7 @@ const RootLayout = ({ children }) => {
                     <Nav />
                     {children}
                 </main>
-
+                </Provider>
             </body>
         </html>
     )
