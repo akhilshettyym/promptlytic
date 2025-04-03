e7c01ed40ce35509307aa6213cc25896146dc77e
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-04-03 20:07:55 +0530

    database added

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,17 +1,31 @@
 import NextAuth from "next-auth";
 import GoogleProvider from 'next-auth/providers/google';
 
+import { connectToDB } from '@utils/database';
 const handler = NextAuth({
     providers: [
         GoogleProvider({
-            clientId: '',
-            clientSecret: '',
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
     async session({ session }) {
-    },
-    async signIn({ profile }) {
 
+    },
+
+    async signIn({ profile }) {
+        try{
+            await connectToDB();
+
+            // Check if a user already exists
+
+            // if not, create a new user
+
+            return true;
+        } catch(error){
+            console.log(error);
+            return false;
+        }
     }
 })
 
